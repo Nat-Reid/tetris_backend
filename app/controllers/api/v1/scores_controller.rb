26e449c0d2c0cd199b546d86969054857a627d95
Tiffany <Tafby88@gmail.com>
2019-04-17 14:40:19 -0700

    posting users and scores in backend

--- app/controllers/api/v1/scores_controller.rb
+++ app/controllers/api/v1/scores_controller.rb
@@ -5,4 +5,17 @@ class Api::V1::ScoresController < ApplicationController
     render json: @scores
   end
 
+  def create
+    # @score = Score.create(score_params)
+    @user = User.find_or_create_by(initials: score_params[:initials])
+    # tiffany.scores.create(score:1200)
+    @user.scores.create(score: score_params[:score])
+    byebug
+    render json: @score
+  end
+
+  private 
+  def score_params
+    params.permit(:score, :initials)
+  end
 end
