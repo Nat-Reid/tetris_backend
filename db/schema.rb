4bff2b91a14590e49ddf81a7c53aec6a9b982326
Tiffany <Tafby88@gmail.com>
2019-04-18 11:36:54 -0700

    created postgres db

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_04_17_165114) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "scores", force: :cascade do |t|
     t.integer "score"
     t.integer "user_id"
