a7c5e717f84c7a16aa7d8d68aacebaa87808dadf
Tiffany <Tafby88@gmail.com>
2019-04-18 11:42:17 -0700

    removed more comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :scores
       resources :users, only: [:create, :show]
-      # get '/profile', to: 'users#profile'
     end
   end
 end
